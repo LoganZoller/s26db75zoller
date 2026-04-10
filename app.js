95fa91ca58b117a522f6fba2289d53e6a94f7999
Logan Zoller <s559751@nwmissouri.edu>
2026-04-10 11:33:57 -0500

    fix?

--- app.js
+++ app.js
@@ -83,6 +83,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
