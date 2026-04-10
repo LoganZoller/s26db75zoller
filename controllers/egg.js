c8023846088b12feb2d405476c22c04568e0e046
Logan Zoller <s559751@nwmissouri.edu>
2026-04-10 10:14:42 -0500

    Updated the Update_put export and changed the route

--- controllers/egg.js
+++ controllers/egg.js
@@ -39,8 +39,21 @@ exports.egg_create_post = async function(req, res) {
 exports.egg_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Egg delete DELETE ' + req.params.id);
 }
-exports.egg_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Egg update PUT' + req.params.id);
+exports.egg_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Egg.findById(req.params.id)
+        if(req.body.bird_type)
+            toUpdate.bird_type = req.body.bird_type;
+        if(req.body.color) toUpdate.color = req.body.color;
+        if(req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Success " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 }
 exports.egg_view_all_Page = async function(req, res) {
     try {
--- routes/egg.js
+++ routes/egg.js
@@ -3,5 +3,5 @@ const egg_controllers = require('../controllers/egg');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/egg/:id', egg_controllers.egg_detail);
+router.get('/egg/:id', egg_controllers.egg_update_put);
 module.exports = router;
