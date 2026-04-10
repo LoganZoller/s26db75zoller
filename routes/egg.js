eee79f23c7cf010e516959139edf313203fd5c48
Logan Zoller <s559751@nwmissouri.edu>
2026-04-10 09:48:45 -0500

    forgot egg/:id

--- routes/egg.js
+++ routes/egg.js
@@ -3,5 +3,5 @@ const egg_controllers = require('../controllers/egg');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', egg_controllers.egg_detail);
+router.get('/egg/:id', egg_controllers.egg_detail);
 module.exports = router;
