098e77af6cc0d7fd7f932080c74d8670cb75ba58
Logan Zoller <s559751@nwmissouri.edu>
2026-04-10 11:25:28 -0500

    issues

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ async function recreateDB() {
     console.error(err);
   }
 }
-let reseed = true;
+let reseed = false;
 if (reseed) {
   recreateDB();
 }
@@ -83,3 +83,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
