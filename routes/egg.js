72f97724d09c591fbd808192597ed044649f1727
Logan Zoller <s559751@nwmissouri.edu>
2026-04-17 02:34:59 -0500

    trying to fix path issues.

--- routes/egg.js
+++ routes/egg.js
@@ -3,7 +3,7 @@ const egg_controllers = require('../controllers/egg');
 var router = express.Router();
 
 /* GET detailed egg page. */
-router.get('/egg', egg_controllers.egg_view_all_Page);
+router.get('/', egg_controllers.egg_view_all_Page);
 
-router.get('/resource/egg/:id/update', egg_controllers.egg_update_Page);
+router.get('/:id/update', egg_controllers.egg_update_Page);
 module.exports = router;
