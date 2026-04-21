00cb64cb95d24fba5624ae30c07fcd0a28707dae
Logan Zoller <s559751@nwmissouri.edu>
2026-04-21 17:04:02 -0500

    Update egg.js

--- routes/egg.js
+++ routes/egg.js
@@ -1,9 +1,20 @@
 var express = require('express');
 const egg_controllers = require('../controllers/egg');
 var router = express.Router();
+var passport = require('passport');
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 /* GET detailed egg page. */
 router.get('/', egg_controllers.egg_view_all_Page);
 
-router.get('/:id/update', egg_controllers.egg_update_Page);
+router.get('/:id/update', secured, egg_controllers.egg_update_Page);
+
+
 module.exports = router;
+
