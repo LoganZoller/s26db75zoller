d3d9477f6036797ee442ebfa82680f4162871a4f
Logan Zoller <s559751@nwmissouri.edu>
2026-04-22 14:16:04 -0500

    added a validation for inputs on the color attribute

--- models/eggSchema.js
+++ models/eggSchema.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const eggSchema = mongoose.Schema({
     bird_type: String,
-    color: String,
+    color: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 10
+    },
     size: String
 });
 
